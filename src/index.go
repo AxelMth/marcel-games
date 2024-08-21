1f41a2d6b3bfee9270ca3f6593308c893b818a05
AxelMth <aujoulator@hotmail.fr>
2024-08-21 12:40:43 +0200

    implement 2 new routes (level WIP)

--- src/index.go
+++ src/index.go
@@ -1,37 +1,162 @@
 package main
 
 import (
-    "context"
-    "fmt"
-
-    "prisma/db"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"prisma/db"
+	"time"
 )
 
-func main() {
-  users := run()
-  fmt.Println(users)
+var client *db.PrismaClient
+
+func init() {
+    var err error
+    client = db.NewClient()
+    if err = client.Prisma.Connect(); err != nil {
+        log.Fatalf("failed to connect to database: %v", err)
+    }
 }
 
-func run() ([]db.UserModel) {
-    client := db.NewClient()
-    var err error
-    if err = client.Prisma.Connect(); err != nil {
-      panic(err)
-    }
-   
+func main() {
     defer func() {
-      if err := client.Prisma.Disconnect(); err != nil {
-        panic(err)
-      }
+        if err := client.Prisma.Disconnect(); err != nil {
+            log.Fatalf("failed to disconnect from database: %v", err)
+        }
     }()
-   
+
+    http.HandleFunc("/launch", launchHandler)
+    http.HandleFunc("/next-level", nextLevelHandler)
+
+    fmt.Println("Starting server at port 8080")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatal(err)
+    }
+}
+
+type LaunchRequest struct {
+    UUID         string `json:"uuid"`
+    Brand        string `json:"brand"`
+    DeviceType   string `json:"deviceType"`
+    IsDevice     bool   `json:"isDevice"`
+    Manufacturer string `json:"manufacturer"`
+    ModelName    string `json:"modelName"`
+    OsName       string `json:"osName"`
+    OsVersion    string `json:"osVersion"`
+}
+
+func launchHandler(w http.ResponseWriter, r *http.Request) {
+    var req LaunchRequest
+    if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+        http.Error(w, "Invalid request payload", http.StatusBadRequest)
+        return
+    }
+
     ctx := context.Background()
-   
-    users, err := client.User.FindMany().Exec(ctx)
+
+    user, err := client.User.UpsertOne(
+        db.User.ID.Equals(req.UUID),
+    ).Update(
+        db.User.LastLogin.Set(time.Now()),
+        db.User.OpenCount.Increment(1),
+    ).Create(
+        db.User.LastLogin.Set(time.Now()),
+        db.User.OpenCount.Set(1),
+    ).Exec(ctx)
 
     if err != nil {
-        panic(err)
+        fmt.Println(err)
+        http.Error(w, "Failed to update user", http.StatusInternalServerError)
+        return
+    }
+    
+
+    _, err = client.UserDevice.UpsertOne(
+        db.UserDevice.UserID.Equals(user.ID),
+    ).Create(
+        db.UserDevice.Brand.Set(req.Brand),
+        db.UserDevice.DeviceType.Set(req.DeviceType),
+        db.UserDevice.IsDevice.Set(req.IsDevice),
+        db.UserDevice.Manufacturer.Set(req.Manufacturer),
+        db.UserDevice.ModelName.Set(req.ModelName),
+        db.UserDevice.OsName.Set(req.OsName),
+        db.UserDevice.OsVersion.Set(req.OsVersion),
+        db.UserDevice.User.Link(
+            db.User.ID.Equals(user.ID),
+        ),
+    ).Update(
+        db.UserDevice.Brand.Set(req.Brand),
+        db.UserDevice.DeviceType.Set(req.DeviceType),
+        db.UserDevice.IsDevice.Set(req.IsDevice),
+        db.UserDevice.Manufacturer.Set(req.Manufacturer),
+        db.UserDevice.ModelName.Set(req.ModelName),
+        db.UserDevice.OsName.Set(req.OsName),
+        db.UserDevice.OsVersion.Set(req.OsVersion),
+    ).Exec(ctx)
+
+    if err != nil {
+        http.Error(w, "Failed to create user device", http.StatusInternalServerError)
+        return
     }
 
-    return users
+    json.NewEncoder(w).Encode(user)
+}
+
+type NextLevelRequest struct {
+    UserID    string `json:"userId"`
+    Attempts  int `json:"attempts"`
+    TimeSpent int `json:"timeSpent"`
+}
+
+func nextLevelHandler(w http.ResponseWriter, r *http.Request) {
+    var req NextLevelRequest
+    if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+        http.Error(w, "Invalid request payload", http.StatusBadRequest)
+        return
+    }
+
+    ctx := context.Background()
+
+    previousLevelHistory, err := client.LevelHistory.FindFirst(
+        db.LevelHistory.UserID.Equals(req.UserID),
+    ).Exec(ctx)
+
+    if err != nil {
+        http.Error(w, "InternalError", http.StatusInternalServerError)
+    }
+
+    var currentLevel int
+    if previousLevelHistory == nil {
+        currentLevel = 1
+    } else {
+        currentLevel = previousLevelHistory.Level + 1
+    }
+
+    rank :=  calculateRank(req.Attempts, req.TimeSpent)
+
+    nextLevel := currentLevel + 1
+    _, err = client.LevelHistory.CreateOne(
+        db.LevelHistory.Level.Set(nextLevel),
+        db.LevelHistory.Attempts.Set(req.Attempts),
+        db.LevelHistory.TimeSpent.Set(req.TimeSpent),
+        db.LevelHistory.Rank.Set(rank),
+        db.LevelHistory.User.Link(
+            db.User.ID.Equals(req.UserID),
+        ),
+    ).Exec(ctx)
+
+    if err != nil {
+        fmt.Println(err)
+        http.Error(w, "Failed to create level history", http.StatusInternalServerError)
+        return
+    }
+
+    response := map[string]int{"next_level": nextLevel, "rank": rank}
+    json.NewEncoder(w).Encode(response)
+}
+
+func calculateRank(attempts, timeSpent int) int {
+    return 100 - (attempts * 2) - (timeSpent / 10)
 }
